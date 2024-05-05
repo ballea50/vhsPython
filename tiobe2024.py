e1e086686f521a270099b192e069d3fcfdd7f449
ballea50 <141548656+ballea50@users.noreply.github.com>
2024-05-05 19:04:44 +0200

    tiobe: random colors, legende links

--- tiobe2024.py
+++ tiobe2024.py
@@ -1,12 +1,20 @@
 import matplotlib.pyplot as plt
+import random
 
 languages = ["Python", "C", "C++", "Java", "C#", "Javascript",
-             "Go", "VB", "SQL", "andere"]
-percent_values = [16.4,10.2,9.8,8.9,6.8,2.9,1.9,1.7,1.6]
+             "Go", "VB", "SQL", "Fortran", "andere"]
+percent_values = [16.4,10.2,9.8,8.9,6.8,2.9,1.9,1.7,1.6, 1.5]
 
 percent_others = 100 - sum(percent_values)
 percent_values.append(percent_others)
+colors = ["#" + ''.join([random.choice('0123456789ABCDEF') for j in range(6)]) for i in range(11)]
 
+print(colors)
 fig, ax = plt.subplots()
-ax.pie(percent_values, labels=languages,shadow=True, autopct='%1.1f%%', startangle=90)
+ax.set_title("TIOBE April 2024")
+#Tortendiagramm
+ax.pie(percent_values, labels=languages,startangle=90, colors=colors)
+#Legende links
+labels = ['{0} - {1:1.1f} %'.format(i, j) for i, j in zip(languages, percent_values)]
+plt.legend(labels, loc='center left', bbox_to_anchor=(-0.35, .5), fontsize=8)
 plt.show()
